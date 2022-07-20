da2f302e2ba609c7135e2c86223601b1367f94a6
themockingjet <jwoot001@gmail.com>
2022-07-21 07:25:05 +0800

    Fix clear inputs

--- resources/javascript/reservation.js
+++ resources/javascript/reservation.js
@@ -1,39 +1,6 @@
 import { collection, doc, setDoc, addDoc } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-firestore.js";
 import { db } from "./firebase.js";
 
-// let btnReserve = document.querySelector("#btnReserve");
-
-// function insertData() {
-// 	let fullName = document.getElementById("firstName").value + " " + document.getElementById("lastName").value;
-// 	let emailAdd = document.getElementById("emailAddress").value;
-// 	let branch = document.getElementById("branch").value;
-// 	let phone = document.getElementById("phone").value;
-// 	let mobile = document.getElementById("mobile").value;
-// 	let date = document.getElementById("date").value;
-// 	let pax = document.getElementById("pax").value;
-// 	let time = document.getElementById("time").value;
-
-// 	addDoc(collection(db, "reservations"), {
-// 		fullname: fullName,
-// 		email: emailAdd,
-// 		branch: branch,
-// 		phone: phone,
-// 		mobile: mobile,
-// 		date: date,
-// 		pax: pax,
-// 		time: time,
-// 		status: "pending",
-// 	})
-// 		.then(() => {
-// 			$("#optionsModal").css({ display: "block" });
-// 		})
-// 		.catch(err => {
-// 			$("#optionsModal").css({ display: "none" });
-// 		});
-// }
-
-// btnReserve.addEventListener("click", insertData);
-
 //======================================================
 // Insert Data + Modal | JJ 07/21/2022
 //======================================================
@@ -73,13 +40,13 @@ $("#btnReserve").on("click", () => {
 $(document).ready(function () {
 	$("button").click("#btnModalOk", function () {
 		$("#optionsModal").css({ display: "none" });
-	});
-
-	$("#optionsModal").on("click", "#btnModalOk", function () {
-		$("input", $("#fieldReservation")).each(function () {
+		$("input", "body").each(function () {
 			this.value = "";
 		});
-		$("#optionsModal").css({ display: "none" });
+	});
+
+	$("#btnReserve").on("click", function () {
+		$("#optionsModal").css({ display: "block" });
 	});
 });
 
