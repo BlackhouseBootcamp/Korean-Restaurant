9893442f6117ee5c473811f467c04029c2a38776
themockingjet <jwoot001@gmail.com>
2022-07-20 10:56:48 +0800

    add admin page

---
+++ admin/resources/importIndexDesignFooterBar.js
@@ -0,0 +1,6 @@
+const getFooterBar = document.querySelector(".footerBar");
+fetch("/resources/html/footer/footerBarDesign.html")
+    .then(res => res.text())
+    .then(data => {
+        getFooterBar.innerHTML = data;
+    });
---
+++ admin/resources/importNavBar.js
@@ -0,0 +1,6 @@
+const getNavigationBar = document.querySelector(".navigationBar");
+fetch("/resources/html/header/navigationBarDesign.html")
+    .then(res => res.text())
+    .then(data => {
+        getNavigationBar.innerHTML = data;
+    });
